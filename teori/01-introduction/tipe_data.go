04f5ecb355bf96d2722d21e9bd96a211afc4c357
akhmadnizar293-sudo <akhmadnizar293@gmail.com>
2026-09-21 13:17:23 +0700

    new file:   teori/01-introduction/tipe_data.go

---
+++ teori/01-introduction/tipe_data.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var umur int8
+	var anakKe int8
+	var saldo int64
+
+	umur = 26
+	anakKe = 3
+	saldo = 260000
+
+	fmt.Println("Umur saya adalah", umur)
+	fmt.Println("Saya adalah anak ke", anakKe)
+	fmt.Println("Saldo saya adalah Rp.", saldo)
+
+	//deklarasi
+	var namaLengkap string
+	//inisialisasi
+	namaLengkap = "Akhmad Nizar"
+	fmt.Println("Nama lengkap saya adalah", namaLengkap)
+}
